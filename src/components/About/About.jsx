2ad498a6277f79b1033c95364b5735cf1f0056b0
stweeeezy <stenlohvart123@gmail.com>
2025-01-16 20:43:19 +0200

    we

---
+++ src/components/About/About.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I’m a frontend developer skilled in creating responsive and
+                high-performance websites.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI/UX experience</h3>
+              <p>
+                I have experience designing landing pages, creating logos, and
+                working on UI elements using Adobe tools and Figma.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Other experience</h3>
+              <p>
+                I have experience working with APIs, managing databases like
+                MySQL, and using Git for version control.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:stenlohvart402@gmail.com">stenlohvart402@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a
+            href="https://linkedin.com/in/sten-lohvart-43242b329/"
+            target="_blank"
+          >
+            linkedin.com/sten-lohvart
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/stweeeezy1" target="_blank">
+            github.com/stweeeezy1
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
