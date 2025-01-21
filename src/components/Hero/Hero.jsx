862e3709b84504d83c73f39f97ec17fcd1ebaba7
stweeeezy <stenlohvart123@gmail.com>
2025-01-21 20:18:19 +0200

    added typing effect

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+  const titleRef = useRef(null);
+  const descriptionRef = useRef(null);
+
+  useEffect(() => {
+    const typedTitle = new Typed(titleRef.current, {
+      strings: ["Hi, I'm Sten"],
+      typeSpeed: 30,
+      startDelay: 100,
+      showCursor: false,
+      onComplete: () => {
+        new Typed(descriptionRef.current, {
+          strings: [
+            "I'm a front-end developer with 1 year of experience using React and JavaScript. Reach out if you'd like to learn more!",
+          ],
+          typeSpeed: 17.5,
+          startDelay: 400,
+          showCursor: false,
+        });
+      },
+    });
+    return () => {
+      typedTitle.destroy();
+    };
+  }, []);
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Sten</h1>
-        <p className={styles.description}>
-          I'm a front-end developer with 1 year of experience using React and
-          JavaScript. Reach out if you'd like to learn more!
-        </p>
+        <h1 className={styles.title} ref={titleRef}></h1>
+        <p className={styles.description} ref={descriptionRef}></p>
         <a href="mailto:stenlohvart402@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
